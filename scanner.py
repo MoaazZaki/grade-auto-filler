12d0caa2c1b8c6e58e4e12ea41f8d5bc112f1f0d
MoaazZaki <moaazzaki1999@gmail.com>
2021-11-30 19:00:22 +0200

    added paper scanner module

---
+++ scanner.py
@@ -0,0 +1,143 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from skimage.filters import threshold_local
+import imutils
+
+def show_images(images,figure,titles=None):
+    #This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
+    # images[0] will be drawn with the title titles[0] if exists
+    # You aren't required to understand this function, use it as-is.
+    n_ims = len(images)
+    if titles is None: titles = ['(%d)' % i for i in range(1,n_ims + 1)]
+    fig = plt.figure(figure)
+    n = 1
+    for image,title in zip(images,titles):
+        a = fig.add_subplot(1,n_ims,n)
+        if image.ndim == 2: 
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+        n += 1
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
+
+
+
+
+class Scanner:
+    """
+    Class used to take captured image and transform it to scanned
+
+    ...
+
+    Attributes
+    ----------
+    says_str : str
+        a formatted string to print out what the animal says
+    name : str
+        the name of the animal
+    sound : str
+        the sound that the animal makes
+    num_legs : int
+        the number of legs the animal has (default 4)
+
+    Methods
+    -------
+    says(sound=None)
+        Prints the animals name and what sound it makes
+    """
+
+    def __init__(self,img,GAUSSIAN_SIZE=(9,9),CANNY_L_THRESH=75,CANNY_H_THRESH=170,DILATION_SIZE=(5,5),DILATION_ITERS=5,EROSION_SIZE=(5,5),EROSION_ITERS=1):
+        self.img = img.copy()
+        self.original = img.copy()
+        self.GAUSSIAN_SIZE = GAUSSIAN_SIZE
+        self.CANNY_L_THRESH = EROSION_ITERS
+        self.CANNY_H_THRESH = CANNY_H_THRESH
+        self.DILATION_SIZE = DILATION_SIZE
+        self.DILATION_ITERS = DILATION_ITERS
+        self.EROSION_SIZE = EROSION_SIZE
+        self.EROSION_ITERS = EROSION_ITERS
+
+    def get_edges(self):
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, self.GAUSSIAN_SIZE, 0)
+        edged = cv2.Canny(gray, self.CANNY_L_THRESH, self.CANNY_H_THRESH)
+        edged = cv2.dilate(edged,self.DILATION_SIZE,iterations=self.DILATION_ITERS)
+        edged = cv2.erode(edged,self.EROSION_SIZE,iterations=self.EROSION_ITERS)
+
+        return edged
+
+    def get_corners(self,edged):
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+        for c in cnts:
+
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+            if len(approx) == 4:
+                corners = approx
+                break
+        
+        return corners
+
+    def order_pts(self,pts):
+        #top-left,top-right,bottom-right, bottom-left
+        rect_pts = np.zeros((4, 2), dtype = "float32")
+        s = pts.sum(axis = 1)
+        rect_pts[0] = pts[np.argmin(s)]
+        rect_pts[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis = 1)
+        rect_pts[1] = pts[np.argmin(diff)]
+        rect_pts[3] = pts[np.argmax(diff)]
+        
+        return rect_pts
+
+    def get_dst_pts(self,rect):
+
+        (tl, tr, br, bl) = rect
+
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype = "float32")
+
+        return dst
+    
+    def trnasform(self,visualize=True):
+        
+        edged = self.get_edges()
+        corners = self.get_corners(edged)
+        ordered_corners = self.order_pts(corners.reshape(-1,2))
+        transformed_corners = self.get_dst_pts(ordered_corners)
+        H = cv2.getPerspectiveTransform(ordered_corners, transformed_corners)
+        transformed = cv2.warpPerspective(self.original, H, (int(transformed_corners[2][0]+1), int(transformed_corners[2][1]+1)))
+
+        if visualize:
+            plt.figure(0)
+            plt.imshow(self.original)
+            plt.figure(1)
+            plt.imshow(edged)
+            cv2.drawContours(self.img, [corners], -1, (0, 255, 0), 15)
+            plt.figure(2)
+            plt.imshow(self.img)
+            plt.figure(3)
+            plt.imshow(transformed)
+            plt.show()
+        return transformed
+
+
+img = cv2.imread('datasets/scanning/2.jpg')
+sc = Scanner(img)
+sc.trnasform()
